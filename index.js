99464bd80306ece2f995ff74b37ce13d26fcb7aa
lcook003 <lcook003@gold.ac.uk>
2025-10-15 11:39:03 +0100

    added read me file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var http = require("http");
 const port = 8000;
 
-//test for github
+//testing version updates
 
 http.createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
