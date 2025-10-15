b9e2904e9a224e417e0e94d5fabce307de85af07
lcook003 <lcook003@gold.ac.uk>
2025-10-15 12:27:07 +0100

    import HTML file to index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // Index.js
 var http = require("http");
+var fs = require("fs");
 const port = 8000;
 
 //testing version updates
 
 http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "html" });
-    res.end("Hello World!");
+    fs.readFile("index.html", function(err, data) {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Error loading index.html");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        }
+    });
+    
 }).listen(port, function() {
     console.log(`Node server is running on port ${port}...`);
 });
